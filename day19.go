a61d3ab3cc5feff628880dfef3678b941cac620a
zhouyangzhi <zhouyangzhi@sudiyi.cn>
2020-04-03 18:29:26 +0800

    add day19

---
+++ day19.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+// 1.下面代码段输出什么？
+type Person struct {
+	age int
+}
+
+func main() {
+	person := &Person{28}
+
+	// 1.
+	defer fmt.Println(person.age)
+
+	// 2.
+	defer func(p *Person) {
+		fmt.Println(p.age)
+	}(person)
+
+	// 3.
+	defer func() {
+		fmt.Println(person.age)
+	}()
+
+	person.age = 29
+}
