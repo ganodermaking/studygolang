e9fd2fa24f217774ae26ddf7f9d4b69cdb4bbf5c
zhouyangzhi <zhouyangzhi@sudiyi.cn>
2020-04-03 10:36:52 +0800

    add day6

---
+++ day6.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+)
+
+type MyInt1 int
+type MyInt2 = int
+
+// 1.通过指针变量p访问其成员变量name，有哪几种方式？
+// A p.name
+// B (&p).name
+// C (*p).name
+// D p->name
+
+// 2.下面这段代码是否能通过编译？如果通过，输出什么？
+func f8() {
+	var i int = 0
+	var i1 MyInt1 = i
+	var i2 MyInt2 = i
+	fmt.Println(i1, i2)
+}
